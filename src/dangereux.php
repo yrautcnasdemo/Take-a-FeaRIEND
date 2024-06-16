58c379518695a835f58dbfc7bc354ad27aa17b67
GregoryVDev <arexo@hotmail.fr>
2024-06-16 20:17:02 +0200

    Added the pictures in desktop and edited width. Added dangereux.php file

--- src/dangereux.php
+++ src/dangereux.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="./css/produits/produits.css">
     <link rel="stylesheet" href="./css/produits/produits-responsive.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <title>Animaux domestiques</title>
+    <title>Animaux PRESQUE adoptables</title>
 </head>
 
 <body>
@@ -37,17 +37,17 @@
         </header>
         <section class="image-illustration"></section>
         <section class="cards-section">
-            <h2>Animaux doméstiques</h2>
+            <h2>Animaux PRESQUE adoptables</h2>
             <div class="container-domestique">
                 <div class="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <article class="container-cards">
                                 <figure>
-                                    <img src="./img/domestiques/0f5fabd8.png" alt="Chat-Dragon">
+                                    <img src="./img/dangereux/105116.png" alt="Serpent-Crocodile">
                                     <figcaption>
                                         <div class="intro-card"><a href="detail.php">
-                                                <h3>Chagon</h3>
+                                                <h3>Serpodile</h3>
                                                 <p>
                                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rerum laudantium, ad deserunt quibusdam corrupti aut, recusandae alias dolores ex expedita quaerat a in et.
                                                 </p>
@@ -60,10 +60,10 @@
                         <div class="carousel-item">
                             <article class="container-cards">
                                 <figure>
-                                    <img src="./img/domestiques/105135.png" alt="Moustique-Migale">
+                                    <img src="./img/dangereux/105155.png" alt="Lion-Serpent">
                                     <figcaption>
                                         <div class="intro-card"><a href="detail.php">
-                                                <h3>Moustigale</h3>
+                                                <h3>Liopent</h3>
                                                 <p>
                                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rerum laudantium, ad deserunt quibusdam corrupti aut, recusandae alias dolores ex expedita quaerat a in et.
                                                 </p>
@@ -76,10 +76,10 @@
                         <div class="carousel-item">
                             <article class="container-cards">
                                 <figure>
-                                    <img src="./img/domestiques/105309.png" alt="Poisson-Méduse">
+                                    <img src="./img/dangereux/105232.png" alt="Aigle-Ours">
                                     <figcaption>
                                         <div class="intro-card"><a href="detail.php">
-                                                <h3>Poiduse</h3>
+                                                <h3>Ougle</h3>
                                                 <p>
                                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rerum laudantium, ad deserunt quibusdam corrupti aut, recusandae alias dolores ex expedita quaerat a in et.
                                                 </p>
@@ -92,7 +92,7 @@
                         <div class="carousel-item">
                             <article class="container-cards">
                                 <figure>
-                                    <img src="./img/domestiques/105356.png" alt="Scolopendre-Loup">
+                                    <img src="./img/dangereux/105417.png" alt="Scorpion-Aigle">
                                     <figcaption>
                                         <div class="intro-card"><a href="detail.php">
                                                 <h3>Scololoup</h3>
@@ -108,7 +108,7 @@
                         <div class="carousel-item">
                             <article class="container-cards">
                                 <figure>
-                                    <img src="./img/domestiques/105644.png" alt="FrelonAsiatique-Migale">
+                                    <img src="./img/dangereux/105446.png" alt="Tigre-Requin">
                                     <figcaption>
                                         <div class="intro-card"><a href="detail.php">
                                                 <h3>Fresiale</h3>
@@ -124,7 +124,7 @@
                         <div class="carousel-item">
                             <article class="container-cards">
                                 <figure>
-                                    <img src="./img/domestiques/105843.png" alt="Requin-Crocodile">
+                                    <img src="./img/dangereux/105605.png" alt="Scorpion-Buffle">
                                     <figcaption>
                                         <div class="intro-card"><a href="detail.php">
                                                 <h3>Croquin</h3>
@@ -144,14 +144,14 @@
 
 
         <section class="desktop-flex">
-            <h2>Animaux doméstiques</h2>
+            <h2>Animaux PRESQUE adoptables</h2>
             <div class="container-flex">
                 <article class="container-cards">
                     <figure>
-                        <img src="./img/domestiques/105135.png" alt="Chat-Dragon">
+                        <img src="./img/dangereux/105116.png" alt="Serpent-Crocodile">
                         <figcaption>
                             <div class="intro-card"><a href="detail.php">
-                                    <h3>Chagon</h3>
+                                    <h3>Serpodile</h3>
                                     <p>
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rerum laudantium, ad deserunt quibusdam corrupti aut, recusandae alias dolores ex expedita quaerat a in et.
                                     </p>
@@ -162,10 +162,10 @@
                 </article>
                 <article class="container-cards">
                     <figure>
-                        <img src="./img/domestiques/105135.png" alt="Moustique-Migale">
+                        <img src="./img/dangereux/105155.png" alt="Lion-Serpent">
                         <figcaption>
                             <div class="intro-card"><a href="detail.php">
-                                    <h3>Moustigale</h3>
+                                    <h3>Liopent</h3>
                                     <p>
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rerum laudantium, ad deserunt quibusdam corrupti aut, recusandae alias dolores ex expedita quaerat a in et.
                                     </p>
@@ -176,10 +176,10 @@
                 </article>
                 <article class="container-cards">
                     <figure>
-                        <img src="./img/domestiques/105309.png" alt="Poisson-Méduse">
+                        <img src="./img/dangereux/105232.png" alt="Aigle-Ours">
                         <figcaption>
                             <div class="intro-card"><a href="detail.php">
-                                    <h3>Poiduse</h3>
+                                    <h3>Ougle</h3>
                                     <p>
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rerum laudantium, ad deserunt quibusdam corrupti aut, recusandae alias dolores ex expedita quaerat a in et.
                                     </p>
@@ -192,7 +192,7 @@
             <div class="container2-flex">
                 <article class="container-cards">
                     <figure>
-                        <img src="./img/domestiques/105356.png" alt="Scolopendre-Loup">
+                        <img src="./img/dangereux/105417.png" alt="Scorpion-Aigle">
                         <figcaption>
                             <div class="intro-card"><a href="detail.php">
                                     <h3>Scololoup</h3>
@@ -206,7 +206,7 @@
                 </article>
                 <article class="container-cards">
                     <figure>
-                        <img src="./img/domestiques/105644.png" alt="FrelonAsiatique-Migale">
+                        <img src="./img/dangereux/105446.png" alt="Tigre-Requin">
                         <figcaption>
                             <div class="intro-card"><a href="detail.php">
                                     <h3>Fresiale</h3>
@@ -220,7 +220,7 @@
                 </article>
                 <article class="container-cards">
                     <figure>
-                        <img src="./img/domestiques/105843.png" alt="Requin-Crocodile">
+                        <img src="./img/dangereux/105605.png" alt="Scorpion-Buffle">
                         <figcaption>
                             <div class="intro-card"><a href="detail.php">
                                     <h3>Croquin</h3>
